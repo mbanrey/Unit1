f9d51542c64d8ca0b8d3129a907db5ca73969bc6
Mitchell <mitchellbanrey@gmail.com>
2022-12-31 17:26:08 -0500

    quick starter

---
+++ Unit_1/porj/index.js
@@ -0,0 +1,42 @@
+//SHonen
+// 5/10 Demon Slayer.
+// 4/10 Spy x Family.
+// 3/10 Chainsaw Man.
+// 2/10 Bleach: Thousand Year Blood War Arc.
+// 1/10 Attack On Titan.
+//SHoujo
+// 5/8 Romantic Killer.
+// 4/8 Kaguya-sama: Love is War (Season 3)
+// 3/8 My Dress-Up Darling.
+// 2/8 Call of the Night.
+// 1/8 Fruits Basket: Prelude.
+// Seinen
+// 5/15 Bungo Stray Dogs.
+// 4/15 Mushi-Shi.
+// 3/15 Fate/Zero.
+// 2/15 Ghost In The Shell.
+// 1/15 Monster.
+// Josei
+// Chihayafuru. 
+// Princess Jellyfish (Kuragehime) 
+// Honey and Clover (Hachimitsu to Clover) 
+// Bunny Drop (Usagi Drop) 
+// Nana.
+
+
+
+
+
+// grab elements from the DOM
+const letters = document.querySelector("#letter_container");
+const options = document.querySelector("#options_container");
+const userInput = document.querySelector("#user_input_container");
+const newGame = document.querySelector("#new_game_container");
+const newGameButton = document.querySelector("#new_game_button");
+const board = document.querySelector("#board")
+const result = document.querySelector("#result_prompt")
+
+//
+let genres = {
+
+}
